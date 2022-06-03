733097aa14ec261defccec07191b208678dd9189
aliza13 <101306734+aliza13@users.noreply.github.com>
2022-06-03 12:59:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,56 @@
 // js code 
 
-var cChoice = ""
-var userSelection = ""
+var cChoice;
+var userSelection;
+var result;
 var handSignals = ["rock", "paper", "scissors"];
 
-function randomChoice() {
-    // grabs random # that = a hand signal
-    cChoice = Math.floor(Math.random(handSignals.length) * 3);
-    cChoice = handSignals[cChoice];
-    //console.log(cChoice)
-    return cChoice;
-}
+var rock = document.getElementById("rock").value;
+var paper = document.getElementById("paper").value;
+var scissors = document.getElementById("scissors").value;
 
-function userChoice(userSelection) {
-    var rock = document.getElementById("rock").value;
-    var paper = document.getElementById("paper").value;
-    var scissors = document.getElementById("scissors").value;
-
-    if (userSelection === rock) {
-        alert("ROCKKK");
-        return userSelection;
-    }
-    else if (userSelection === paper) {
-        alert("pp");
-        return userSelection;
-    }
-    else if (userSelection === scissors) {
-        alert("scissorss")
-        return userSelection;
-    }
-}
-
-function compareHands(compChoice, userChoice) {
+function compareHands(userSelection, cChoice) {
+    
     // how to I call this function in my html after the user and computer make a choice?
-    if (compChoice === rock && userChoice === rock || compChoice === paper && userChoice === paper || compChoice === scissors && userChoice === scissors) {
-        alert("TIE")
+    if (cChoice === rock && userSelection === rock || cChoice === paper && userSelection === paper || cChoice === scissors && userSelection === scissors) {
+        return result = "tie"
     }
-    else if(compChoice === paper && userChoice === rock || compChoice === scissors && userChoice === paper || compChoice === rock && userChoice === scissors) {
-        alert("computer wins")
+    else if(cChoice === paper && userSelection === rock || cChoice === scissors && userSelection === paper || cChoice === rock && userSelection === scissors) {
+        return result = "Computer Wins"
     }
-    else if(compChoice === paper && userChoice === scissors || compChoice === scissors && userChoice === rock || compChoice === rock && userChoice === paper) {
-        alert("User wins")
+    else if(cChoice === paper && userSelection === scissors || cChoice === scissors && userSelection === rock || cChoice === rock && userSelection === paper) {
+        return result = "User Wins"
     }
+
+    function userChoice(userSelection) {
+        var rock = document.getElementById("rock").value;
+        var paper = document.getElementById("paper").value;
+        var scissors = document.getElementById("scissors").value;
+    
+        if (userSelection === rock) {
+            return userSelection;
+        }
+        else if (userSelection === paper) {
+            return userSelection;
+        }
+        else if (userSelection === scissors) {
+            // console.log(userSelection); // it works! returns scissors
+            return userSelection;
+        }
+    }
+    function randomChoice() {
+        // grabs random # that = a hand signal
+        cChoice = Math.floor(Math.random(handSignals.length) * 3);
+        cChoice = handSignals[cChoice];
+        //console.log(cChoice) works!
+        return cChoice;
+    }
+    
+    userChoice(userSelection);
+    randomChoice();
+    // console.log(userChoice(userSelection) + randomChoice()); This Works!!!
 }
 
-
-
-randomChoice();
-
-userChoice(userSelection);
+compareHands(userSelection, cChoice);
+console.log(result) // nothing appears
+// var results = document.write(result);
