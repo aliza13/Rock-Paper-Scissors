c1b290081a75622801b2b3d2475c480a152c5659
aliza13 <101306734+aliza13@users.noreply.github.com>
2022-06-05 14:11:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,11 @@ var userSelection;
 var result;
 var handSignals = ["rock", "paper", "scissors"];
 
-var rock = document.getElementById("rock").value;
-var paper = document.getElementById("paper").value;
-var scissors = document.getElementById("scissors").value;
+var rock = "rock";
+var paper = "paper";
+var scissors = "scissors";
 
 function compareHands(userSelection, cChoice) {
-    
-    // how to I call this function in my html after the user and computer make a choice?
     if (cChoice === rock && userSelection === rock || cChoice === paper && userSelection === paper || cChoice === scissors && userSelection === scissors) {
         return result = "tie"
     }
@@ -21,36 +19,20 @@ function compareHands(userSelection, cChoice) {
     else if(cChoice === paper && userSelection === scissors || cChoice === scissors && userSelection === rock || cChoice === rock && userSelection === paper) {
         return result = "User Wins"
     }
-
-    function userChoice(userSelection) {
-        var rock = document.getElementById("rock").value;
-        var paper = document.getElementById("paper").value;
-        var scissors = document.getElementById("scissors").value;
-    
-        if (userSelection === rock) {
-            return userSelection;
-        }
-        else if (userSelection === paper) {
-            return userSelection;
-        }
-        else if (userSelection === scissors) {
-            // console.log(userSelection); // it works! returns scissors
-            return userSelection;
-        }
-    }
-    function randomChoice() {
-        // grabs random # that = a hand signal
-        cChoice = Math.floor(Math.random(handSignals.length) * 3);
-        cChoice = handSignals[cChoice];
-        //console.log(cChoice) works!
-        return cChoice;
-    }
-    
-    userChoice(userSelection);
-    randomChoice();
-    // console.log(userChoice(userSelection) + randomChoice()); This Works!!!
+    console.log(result);  // does not work
 }
 
-compareHands(userSelection, cChoice);
-console.log(result) // nothing appears
-// var results = document.write(result);
+function userChoice(userSelection) {
+    // ???
+    return compareHands(userSelection, randomChoice());
+}
+
+
+function randomChoice() {
+    // grabs random # that = a hand signal
+    cChoice = Math.floor(Math.random(handSignals.length) * 3);
+    cChoice = handSignals[cChoice];
+    return cChoice;
+}
+
+alert(result)  // does not work
